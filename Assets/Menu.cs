1eef89385e801edfaf7b6dac50a56b908b883a26
Ali farhad <alifarhadali990@gmail.com>
2022-03-19 19:17:08 +0300

    data between scenes are done

---
+++ Assets/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; using UnityEngine.SceneManagement; using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Text text;
+    public InputField input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        DataManager.Instance.playerName = input.text;
+       SceneManager.LoadScene("main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
